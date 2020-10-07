29e4bf74a49e70eb746f363d116395e916cf77b2
mcamargo <manuelc@sipios.com>
2020-10-07 19:06:44 +0200

    First commit

---
+++ src/main/java/com/gmstandingssim/gmstandingsapi/GmStandingsApiApplication.java
@@ -0,0 +1,13 @@
+package com.gmstandingssim.gmstandingsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmStandingsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmStandingsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmstandingssim/gmstandingsapi/GmStandingsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmstandingssim.gmstandingsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmStandingsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
