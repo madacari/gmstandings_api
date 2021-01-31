aceaab26362a090332c319f702bd0d582f185894
mcamargo <manuelc@sipios.com>
2021-01-31 12:54:31 +0100

    feat: add cors policy

--- src/main/java/com/gmstandingssim/gmstandingsapi/GmStandingsApiApplication.java
+++ src/main/java/com/gmstandingssim/gmstandingsapi/GmStandingsApiApplication.java
@@ -2,6 +2,9 @@ package com.gmstandingssim.gmstandingsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GmStandingsApiApplication {
@@ -9,5 +12,18 @@ public class GmStandingsApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GmStandingsApiApplication.class, args);
 	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/tournament-data")
+						.allowedOrigins("http://localhost:3000",
+								"https://youthful-goldberg-afc5cf.netlify.app");
+			}
+		};
+	}
+
 
 }
